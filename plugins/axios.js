b0ba725e564f29c2eeda18875789fc7da883e672
Jake Medler <65961686+JakeLewisMedler@users.noreply.github.com>
2025-02-27 10:23:48 +0000

    add axios auth back in

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,13 +4,13 @@ export default (ctx, inject) => {
   const instance = axios.create({
     baseURL: ctx.$config.API_URL
   });
-  // instance.interceptors.request.use(async (config) => {
-  //   let token = await ctx.$firebase.getIdToken();
-  //   let value = "Bearer " + token;
-  //   config.headers["Authorization"] = value;
+  instance.interceptors.request.use(async (config) => {
+    let token = await ctx.$firebase.getIdToken();
+    let value = "Bearer " + token;
+    config.headers["Authorization"] = value;
 
-  //   return config;
-  // });
+    return config;
+  });
 
   instance.interceptors.response.use((response) => {
     return response.data;
